82ba55324ec6b4d95a1f0e5581af3837bf6c0d8b
whaleflyingsky <sunbeanp@hanyang.ac.kr>
2023-10-16 03:34:12 +0900

    231015 Algorithm

---
+++ UNIT03-배열/10808.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b, c, mul;
+    string mul_str;
+    int num[10];
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    mul = a * b * c;
+    mul_str = to_string(mul);
+//    cout << mul_str << " " << mul_str[2];
+    for (int i=0; i<=9; i++) {
+    
+    }
+}
---
+++ UNIT03-배열/2577.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, v, result;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    cin >> v;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == v) {
+            result++;
+        }
+    }
+    cout << result;
+}
